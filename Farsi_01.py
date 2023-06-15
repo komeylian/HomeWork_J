ddbab10ec69995497c6dc42f89d426ca1117c1f1
unknown <komeylian@gmail.com>
2023-06-16 01:33:40 +0330

    For School Mate 1402-03-26

--- Farsi_01.py
+++ Farsi_01.py
@@ -1,19 +1,30 @@
-# 1402-03-25
-
+#1402-03-26
 import arabic_reshaper
 from bidi.algorithm import get_display
 
 
 def fprint(text='سلام من کمیلیان هستم'):
-    persian_text = text
-    reshaped_text = arabic_reshaper.reshape(persian_text)
-    bidi_text = get_display(reshaped_text)
-    # print(bidi_text)
-    return bidi_text
+    if not isinstance(text, str):
+        return text
 
+    try:
+        reshaped_text = arabic_reshaper.reshape(text)
+        display_text = get_display(reshaped_text)
+        return display_text
+    except arabic_reshaper.ArabicReshaperError:
+        return text
+    
 
-print(fprint())
+print(fprint(), '\n')
 
-print()
+# print(F'\n')
 
 print(fprint('سلام من یک فانکشن هستم'))
+# Example usage:
+farsi_text = "این یک متن فارسی است"
+english_text = "This is an English text"
+mixed_text = "این یک متن فارسی است. This is an English text."
+
+print(fprint(farsi_text))    # این یک متن فارسی است
+print(fprint(english_text))  # This is an English text
+print(fprint(mixed_text))    # این یک متن فارسی است. This is an English text.
